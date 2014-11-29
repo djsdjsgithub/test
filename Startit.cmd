a2605108f3fdce9c86eaf608d7a9c7b142dad4c8
djsdjsgithub <djsdjsgithub.services@sanoys.com>
2014-11-28 18:52:44 -0800

    Update Startit.cmd

--- Startit.cmd
+++ Startit.cmd
@@ -1,4 +1,3 @@
-InstallChocolateyandBoxStarter.cmd
 Echo Getting Started...
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 
