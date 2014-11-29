e325affa40d1c87904f6e2c69ff6da96722626c3
djsdjsgithub <djsdjsgithub.services@sanoys.com>
2014-11-29 09:04:59 -0500

    Update startit.ps1

--- startit.ps1
+++ startit.ps1
@@ -4,6 +4,7 @@ $env:path = "$($env:ALLUSERSPROFILE)\chocolatey\bin;$($env:Path)"
 
 "Chocolately Installed, Now Installing BoxStarter" | out-default
 choco install BoxStarter
+$env:path = "$($env:APPDATA)\BoxStarter;$($env:Path)"
 
 "Installing QP Demo Configuration" | out-default
 Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/djsdjsgithub/test/master/QPDemo.txt
