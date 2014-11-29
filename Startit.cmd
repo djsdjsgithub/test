4bd7275e3411a39a564dca817b490674276f87c6
djsdjsgithub <djsdjsgithub.services@sanoys.com>
2014-11-28 20:18:37 -0500

    Create Startit.cmd

---
+++ Startit.cmd
@@ -0,0 +1,9 @@
+InstallChocolateyandBoxStarter.cmd
+Echo Getting Started...
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+
+Echo Chocolately Installed, Now Installing BoxStarter
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "choco install BoxStarter"
+
+Echo Installing QP Demo Configuration
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/djsdjsgithub/test/master/QPDemo.txt"
