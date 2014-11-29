228b719a105b8c25515b34aa1c8ec31828e1e629
djsdjsgithub <djsdjsgithub.services@sanoys.com>
2014-11-29 09:29:40 -0500

    Update startit.ps1

--- startit.ps1
+++ startit.ps1
@@ -1,10 +1,11 @@
 "Getting Started..." | out-default
+set-executionpolicy bypass -force
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')) 
 $env:path = "$($env:ALLUSERSPROFILE)\chocolatey\bin;$($env:Path)"
 
 "Chocolately Installed, Now Installing BoxStarter" | out-default
 choco install BoxStarter
-$env:path = "$($env:APPDATA)\BoxStarter;$($env:Path)"
+."$env:APPDATA\BoxStarter\BoxStarterShell.ps1"
 
 "Installing QP Demo Configuration" | out-default
 Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/djsdjsgithub/test/master/QPDemo.txt
