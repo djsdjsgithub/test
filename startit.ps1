19d12e46ef41038704c34081847d8f36ae5fc959
djsdjsgithub <djsdjsgithub.services@sanoys.com>
2014-11-29 08:40:37 -0500

    Create startit.ps1

---
+++ startit.ps1
@@ -0,0 +1,9 @@
+"Getting Started..." | out-default
+iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" 
+$env:path = "$env:ALLUSERSPROFILE\chocolatey\bin;$env:Path"
+
+"Chocolately Installed, Now Installing BoxStarter" | out-default
+choco install BoxStarter
+
+"Installing QP Demo Configuration" | out-default
+Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/djsdjsgithub/test/master/QPDemo.txt
